16aa6682144f683fe368db47814d0be4efded8c0
Andrea <79668261+Andrestii@users.noreply.github.com>
2024-03-05 17:53:23 +0100

    Fixed file location

---
+++ Dino Jump Andrea Onesti/UserData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dino_Jump_Andrea_Onesti
+{
+    public class UserData
+    {
+        public string Username { get; set; }
+        public int Points { get; set; }
+
+        public UserData() { }
+        public UserData(string iUsername, int iPoints)
+        {
+            Username = iUsername;
+            Points = iPoints;
+        }
+    }
+}
